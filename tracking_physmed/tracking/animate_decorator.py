cdcc5b02023480b8530c820adc68f029b603b6e9
Felipe Cybis Pereira <felipe.cybis-pereira@espci.psl.eu>
2023-02-23 19:50:04 +0100

    need to change animation class, adding structure

--- tracking_physmed/tracking/animate_decorator.py
+++ tracking_physmed/tracking/animate_decorator.py
@@ -72,6 +72,191 @@ def anim_decorator(plot_function):
     return plot_wrapper
 
 
+# class _Animate_plot(Animation):
+#     def __init__(
+#         self,
+#         figure,
+#         video_path=None,
+#         x_crop=[0, -1],
+#         y_crop=[0, -1],
+#         interactive=True,
+#         show_timestamp=True,
+#         position_timestamp=[0, 0, 0.5, 0.5],
+#         other_artists=[],
+#     ):
+#         """Animation of 2/3D+t scans. This obviously needs interactive backend to work.
+#         This class was not built to direct use. Please use `animate_scan(scan)` to animate 2D+t or 3D+t scans.
+#         If `interactive=True`, then this are the controls for the animation:
+#             `backspace` -> play/pause
+#             `up/down` -> adjusts the frame step of the animation (default and minimum value is 1 to grab the next frame, if set to 2, will skip 1 frame, and so on...
+#             `+/-` -> adjusts the interval between frames in ms (default is 200), it will multiply or divide by 2 if used + or -, respectively
+#         """
+#         ## Creating custom animation inheriting matplotlib Animation class
+#         self.is_playing = True
+
+#         # frame_step says if animation is going frame by frame (frame_step = 1), or if it is going to skip one frame (frame_step = 2), etc.
+#         # if sampling frequency of the animation data is high, sometimes skipping some frames is a good trade-off to have a smoother animation
+#         self.frame_step = 1
+#         self.current_frame = 0
+
+#         self.anim_interval = 50
+#         event_source = figure.canvas.new_timer(interval=self.anim_interval)
+
+
+#         # self._framedata = range(0, self.n_frames, self.frame_step)
+#         self._drawn_artists = []
+
+#         super().__init__(fig=figure, event_source=event_source, blit=True)
+#         # self._drawn_artists += images
+
+#         self.show_timestamp = show_timestamp
+#         if show_timestamp:
+#             # Adding an axes for counting the frames
+#             ax_xlabel = figure.add_axes(position_timestamp, frameon=False)
+#             ax_xlabel.xaxis.set_visible(False)
+#             ax_xlabel.yaxis.set_visible(False)
+#             self.time_stamp = ax_xlabel.text(
+#                 0.5,
+#                 0.5,
+#                 "current frame: {fr:04} | time: {sec:07.2f} s".format(
+#                     fr=self.current_frame, sec=self.data_time[self.current_frame]
+#                 ),
+#                 transform=ax_xlabel.transAxes,
+#                 ha="center",
+#                 va="top",
+#                 bbox=dict(facecolor="white", alpha=1, edgecolor="white"),
+#             )
+#             self._drawn_artists.append(self.time_stamp)
+#         if other_artists:
+#             self._drawn_artists.append(*other_artists)
+
+#         # interval limit is the minimum time delay between frames (in ms)
+#         # set anim_interval to 1 does not mean it is really going to update frames every 1 ms, but rather it will update as fast as possible
+#         self.interval_limit = 1
+#         if not self._blit:
+#             self.interval_limit = 100
+#             self.anim_interval = 200
+#             warnings.warn(
+#                 "Matplotlib figure does not support blit. Animation will not be optimal. This happens because the backend used does not support matplotlib blitting."
+#             )
+
+#         if interactive is False:
+#             # just starts animation and let it playing
+#             self.play()
+#         else:
+#             self.ckeypress = figure.canvas.mpl_connect(
+#                 "key_press_event", self.onkeypress
+#             )
+
+#     def _start(self, *args):
+#         """
+#         Starts interactive animation. Adds the draw frame command to the GUI
+#         handler, calls show to start the event loop.
+#         """
+#         # Do not start the event source if saving() it.
+#         if self._fig.canvas.is_saving():
+#             return
+#         # First disconnect our draw event handler
+#         self._fig.canvas.mpl_disconnect(self._first_draw_id)
+
+#         # Now do any initial draw
+#         self._init_draw()
+
+#         # Add our callback for stepping the animation and
+#         # actually start the event_source.
+#         self.event_source.add_callback(self._step)
+#         self.event_source.start()
+
+#     def _step(self):
+#         try:
+#             self.current_frame = next(self.frame_seq)
+#             self._draw_next_frame(self.current_frame, self._blit)
+#         except StopIteration:
+#             # modified from default to restart animation when iterator ends
+#             self.frame_seq = self.new_frame_seq()
+
+#     def _draw_frame(self, framedata):
+#         # handles the next frame, if outside of the time dimension, goes back to the beginning
+#         for data, im in zip(self.datas, self.images):
+#             im.set_array(data[..., framedata])
+#         if self.show_timestamp:
+#             self.time_stamp.set_text(
+#                 "current frame: {fr:04} | time: {sec:07.2f} s".format(
+#                     fr=framedata, sec=self.data_time[framedata]
+#                 )
+#             )
+
+#     def _on_resize(self, event):
+#         # On resize, we need to disable the resize event handling so we don't
+#         # get too many events. Also stop the animation events, so that
+#         # we're paused. Reset the cache and re-init. Set up an event handler
+#         # to catch once the draw has actually taken place.
+#         self._fig.canvas.mpl_disconnect(self._resize_id)
+#         # slightly modified from matplotlib default where if animation is paused,
+#         # resizing it would resume playing. Now it only resumes playing if it
+#         # was already playing
+#         if self.is_playing:
+#             self.pause()
+#         self._blit_cache.clear()
+#         self._init_draw()
+#         self._resize_id = self._fig.canvas.mpl_connect("draw_event", self._end_redraw)
+
+#     def _end_redraw(self, event):
+#         # Now that the redraw has happened, do the post draw flushing and
+#         # blit handling. Then re-enable all of the original events.
+#         self._post_draw(None, False)
+#         # slightly modified from matplotlib default where if animation is paused,
+#         # resizing it would resume playing. Now it only resumes playing if it
+#         # was already playing
+#         if self.is_playing:
+#             self.resume()
+#         self._fig.canvas.mpl_disconnect(self._resize_id)
+#         self._resize_id = self._fig.canvas.mpl_connect("resize_event", self._on_resize)
+
+#     def onkeypress(self, event):
+#         # animation controls
+#         if event.key == " ":
+#             self.play()
+
+#         elif event.key == "-":
+#             self.anim_interval *= 2
+#             self.event_source.interval = int(self.anim_interval)
+#         elif event.key == "+":
+#             if self.anim_interval <= self.interval_limit:
+#                 pass
+#             else:
+#                 self.anim_interval /= 2
+#         elif event.key == "up":
+#             self.frame_step += 1
+#             self._framedata = range(self.current_frame, self.n_frames, self.frame_step)
+#             self.frame_seq = self.new_frame_seq()
+#         elif event.key == "down":
+#             if self.frame_step > 1:
+#                 self.frame_step -= 1
+#                 self._framedata = range(
+#                     self.current_frame, self.n_frames, self.frame_step
+#                 )
+#                 self.frame_seq = self.new_frame_seq()
+
+#         print(
+#             "Frame step:",
+#             self.frame_step,
+#             "Timer interval:",
+#             int(self.anim_interval),
+#             "msec",
+#         )
+#         self.event_source.interval = int(self.anim_interval)
+
+#     def play(self):
+#         # simply toggle between play/pause
+#         if self.is_playing:
+#             self.pause()
+#             self.is_playing = False
+
+#         else:
+#             self.is_playing = True
+#             self.resume()
+
 class Animate_plot:
     def __init__(self, fig, ax, video_path=None, x_crop=[0, -1], y_crop=[0, -1]):
 
